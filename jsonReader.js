366422a9f26c210c33bf11131ee620e45a214b14
Unknown <converse1337@gmail.com>
2017-07-29 14:42:40 +1000

    More more json reading

--- jsonReader.js
+++ jsonReader.js
@@ -1,13 +1,26 @@
-var obj = require("C:\\Users\\Reuben\\Documents\\Politech\\politech\\reps.json");
+let obj = require("C:\\Users\\Reuben\\Documents\\Politech\\politech\\reps.json");
 
+let past100 = require("C:\\Users\\Reuben\\Documents\\Politech\\politech\\past100.json");
+
+/*var request = require('request');
+request('https://theyvoteforyou.org.au/api/v1/divisions.json?key=qB79nIe4HcQHOKGHrj6o', 
+	getJSON);
+*/
 /*
 for (var exKey in obj) {
 	console.log("key:" +exKey+", value:"+obj[exKey]);
 }
 */
 //getDescription(obj);
-getDivisionResult(obj, "Malcolm", "Turnbull");
-getDescription(obj);
+//getDivisionResult(obj, "Malcolm", "Turnbull");
+//getDescription(obj);
+console.log(getDivision("2017-06-21"));
+
+function getJSON(error, response, body){
+	json = JSON.parse(body);
+	console.log(json);
+}
+
 function getDescription(jsonFile){
 	console.log(obj.summary);
 }
@@ -25,3 +38,22 @@ function getDivisionResult(jsonFile, firstName, secondName){
 	//console.log(jsonFile.votes["100"].member.first_name);
 	
 }
+
+function getDivision(dateString) {
+	let ids = [];
+	let names = [];
+	for (var key in past100) {
+		if(past100[key].date == dateString) {
+			ids.push(past100[key].id);
+			names.push(past100[key].name);
+		}
+	}
+	let urls = [];
+	for (var id in ids) {
+		urls.push("https://theyvoteforyou.org.au/api/v1/divisions/" + ids[id] + ".json?key=qB79nIe4HcQHOKGHrj6o");
+	}
+	
+	return urls;
+
+}
+
