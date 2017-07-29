955c4dd9e5780cc1198b1dfaf892423b7051e9cf
Unknown <converse1337@gmail.com>
2017-07-29 12:09:19 +1000

    More JSON reading

--- jsonReader.js
+++ jsonReader.js
@@ -1,18 +1,27 @@
-var obj = require("C:\\Users\\Reuben\\Documents\\Politech\\politech\\test.json");
+var obj = require("C:\\Users\\Reuben\\Documents\\Politech\\politech\\reps.json");
 
-
+/*
 for (var exKey in obj) {
 	console.log("key:" +exKey+", value:"+obj[exKey]);
 }
-
-
+*/
+//getDescription(obj);
+getDivisionResult(obj, "Malcolm", "Turnbull");
+getDescription(obj);
 function getDescription(jsonFile){
-	for (var key in jsonFile) {
-		//console.log("key: " + key);
-		if (key == "summary") {
-			console.log(obj[key])
-		}
-	}
+	console.log(obj.summary);
 }
 
 
+function getDivisionResult(jsonFile, firstName, secondName){
+	for (var key in jsonFile.votes){
+		if (jsonFile.votes[key].member.first_name == firstName &&
+			jsonFile.votes[key].member.last_name == secondName) {
+			console.log(jsonFile.votes[key].vote);
+		}
+		
+		
+	}
+	//console.log(jsonFile.votes["100"].member.first_name);
+	
+}
